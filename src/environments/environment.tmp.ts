8a058e74a4d88a3208c9628f8a376fdb7a588298
nanafranck <nanafranck94@gmail.com>
2025-06-04 19:50:04 +0200

    modification de la ci cd et ajout du fichier environment.tmp.ts

--- src/environments/environment.tmp.ts
+++ src/environments/environment.tmp.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyBlEs5Fq0S12DZYnqKPu05_25EfH7-Dz7w",
-    authDomain: "school-management-af62e.firebaseapp.com",
-    projectId: "school-management-af62e",
-    storageBucket: "school-management-af62e.firebasestorage.app",
-    messagingSenderId: "1075453371712",
-    appId: "1:1075453371712:web:39b287b4defe141a6006a5",
-    measurementId: "G-WJNC5LTT9Q"
-  }
+  firebaseConfig: {}
 };
